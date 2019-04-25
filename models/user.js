a337635725d58fa01cacd875e3f6a41960e45fa1
JDVapor <jdvapordestiny@gmail.com>
2019-04-25 11:50:54 -0700

    added user validations

--- models/user.js
+++ models/user.js
@@ -1,6 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
-    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dob: DataTypes.DATE,
     bio: DataTypes.TEXT,
     rewards: DataTypes.INTEGER
   });
